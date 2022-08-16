a22d5107a65b071c31e58fdcde098a13ffb44dc1
PacoReinaCampo <frareicam@gmail.com>
2022-08-16 07:46:15 +0200

    Working in PU-FinTech 🦀

--- doc/write.sh
+++ doc/write.sh
@@ -1,5 +1,8 @@
 rm -f *.pdf
 rm -f *.tex
 
+pandoc PRELUDE-TT.md -s -o PU-TT-NTM.pdf
 pandoc BOOK.md -s -o PU-FinTech.pdf
+
+pandoc PRELUDE-TT.md -s -o PU-TT-NTM.tex
 pandoc BOOK.md -s -o PU-FinTech.tex
